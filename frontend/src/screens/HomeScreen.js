106bcab8b0c6c628d0348c06782cd5bb36f53704
zayedibnibrahim <zayed.ibn@gmail.com>
2021-06-13 05:19:18 +0600

    Setup backend

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,8 +1,10 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Row, Col } from 'react-bootstrap'
 import Product from '../components/Product'
 import products from '../products'
 const HomeScreen = () => {
+  //   const [products, setProducts] = useState([])
+  //   useEffect(() => {}, [])
   return (
     <>
       <h1>Latest Products</h1>
