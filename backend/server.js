6c203fed5b82bf52963859f1e0b8c48ab0254fba
zayedibnibrahim <zayed.ibn@gmail.com>
2021-06-13 19:05:52 +0600

    setup mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const dotenv = require('dotenv')
+const colors = require('colors')
+const connectDB = require('./config/db')
 dotenv.config()
 
 const app = express()
 
+connectDB()
 const PORT = process.env.PORT || 5000
-app.listen(PORT, console.log('Server running at 5000'))
+app.listen(PORT, console.log('Server running at 5000'.yellow.bold))
