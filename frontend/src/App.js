d87ef5856948e6aa720cc285797904cdb89c4ead
zayedibnibrahim <zayed.ibn@gmail.com>
2021-06-19 06:05:41 +0600

    login screen added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import { Container } from 'react-bootstrap'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import ProductScreen from './screens/ProductScreen'
 import CartScreen from './screens/CartScreen'
+import LoginScreen from './screens/LoginScreen'
 
 function App() {
   return (
@@ -12,9 +13,10 @@ function App() {
       <Header></Header>
       <main className='py-3'>
         <Container>
-          <Route path='/' component={HomeScreen} exact />
+          <Route path='/login' component={LoginScreen} />
           <Route path='/product/:id' component={ProductScreen} />
           <Route path='/cart/:id?' component={CartScreen} />
+          <Route path='/' component={HomeScreen} exact />
         </Container>
       </main>
       <Footer></Footer>
--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -10,7 +10,7 @@ export const userLoginReducer = (state = {}, action) => {
     case USER_LOGIN_REQUEST:
       return { loading: true }
     case USER_LOGIN_SUCCESS:
-      return { loading: false, userData: action.payload }
+      return { loading: false, userInfo: action.payload }
     case USER_LOGIN_FAIL:
       return { loading: false, error: action.payload }
     case USER_LOGOUT:
