885de1a4d52faf3e03e31f41ea3bebc90f2dad0b
zayedibnibrahim <zayed.ibn@gmail.com>
2021-06-22 06:11:41 +0600

    Added place order screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,6 +10,7 @@ import RegisterScreen from './screens/RegisterScreen'
 import ProfileScreen from './screens/profileScreen'
 import ShippingScreen from './screens/ShippingScreen'
 import PaymentScreen from './screens/PaymentScreen'
+import PlaceOrderScreen from './screens/PlaceOrderScreen'
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
       <Header></Header>
       <main className='py-3'>
         <Container>
+          <Route path='/placeorder' component={PlaceOrderScreen} />
           <Route path='/payment' component={PaymentScreen} />
           <Route path='/shipping' component={ShippingScreen} />
           <Route path='/profile' component={ProfileScreen} />
